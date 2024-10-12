c2876badc15e927110f7a9a3472bb8c8bde4f37c
TamTran <tamttt14@fpt.edu.vn>
2024-10-12 13:02:50 +0700

    Add root route

--- db.js
+++ db.js
@@ -1,7 +1,16 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/studentsdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB', err));
+  .catch((err) => console.error('Error connecting to MongoDB', err));*/
+  const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Could not connect to MongoDB...", err));
+
